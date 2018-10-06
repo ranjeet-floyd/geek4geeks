a129c464a0597239a442681cd6ab011e01d1fd57
Kumar, Ranjeet <ranjeet.kumar@rakuten.com>
2018-10-07 01:31:53 +0530

    UF application and quiz

---
+++ random/algorithm/Perculation.java
@@ -0,0 +1,9 @@
+/**
+ * @author ranjeet
+ * @see https://www.coursera.org/learn/algorithms-part1/lecture/OLXM8/union-find-applications
+ */
+public class Perculation {
+    public static void main(String[] args) {
+        System.out.println("Hello perculation, ");
+    }
+}
